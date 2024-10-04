bdadaca87724ec66d6bbc6746fecdd2e5da8c818
Im-Jota <joseluisromeroarce12@gmail.com>
2024-10-04 17:14:27 -0500

    TaskController-Commit

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -33,6 +33,71 @@ class TaskController{
       })
     })
   }
+
+  getTask(req, res) {
+    const id = parseInt(req.url.split('/')[2]);
+    this._task.getTask(id, (error, data) => {
+      if(error) console.log(error);
+      res.writeHeader(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(data));
+    })
+  }
+
+  updateName(req, res) {
+    let data = '';
+    const id = req.url.split('/')[2];
+    req.on('data', (buffer) => {
+      data += buffer.toString();
+    });
+    req.on('end', () => {
+      const { name } = JSON.parse(data);
+      this._task.updateName(name, id, (error, data) => {
+        if (error) {
+          res.statusCode = '500';
+          res.setHeader('Content-Type', 'text/plain');
+          res.write(error);
+        } else {
+          res.writeHeader(200, {'Content-Type': 'text/plain'});
+          res.end('Exito')
+        }
+      })
+    })
+  }
+
+  updateState(req,res) {
+    let data = '';
+    const id = req.url.split('/')[2];
+    req.on('data', (buffer) => {
+      data += buffer.toString();
+    });
+    req.on('end', () => {
+      const { state } = JSON.parse(data);
+      this._task.updateState(state, id, (error, data) => {
+        if (error) {
+          res.statusCode = '500';
+          res.setHeader('Content-Type', 'text/plain');
+          res.write(error);
+        } else {
+          res.writeHeader(200, {'Content-Type': 'text/plain'});
+          res.end('Exito')
+        }
+      })
+    })
+  }
+
+  deleteTask(req, res) {
+    const id = req.url.split('/')[2];
+    this._task.deleteTask(id, (error, data) => {
+      if(error) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write(error);
+      } else {
+        res.writeHeader(200, {'Content-Type': 'text/plain'});
+        res.end('exito');
+      }
+    })
+  }
 }
 
 
