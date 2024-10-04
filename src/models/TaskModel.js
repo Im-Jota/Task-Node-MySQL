6f1e500936a5a7bc6bc58841fb87df950e0a7573
Im-Jota <joseluisromeroarce12@gmail.com>
2024-10-04 11:23:28 -0500

    TaskModel-commit

--- src/models/TaskModel.js
+++ src/models/TaskModel.js
@@ -24,6 +24,39 @@ class Task {
       }
     })
   }
+
+  getTask(id, callback) {
+    const select = `select * from ${this._table} where id = ${id}`;
+    connection.query(select, (error, data) => {
+      if (error) callback(error, null);
+      callback(null, data);
+    })
+  }
+
+  updateName(task, id, callback) {
+    const update = 'update task set name = ?, udpate_at = ? where id = ?';
+    const now = new Date();
+    connection.query(update, [task, now, id], (error, data) => {
+      if (error) callback(error, null);
+      callback(null, data);
+    })
+  }
+
+  updateState(state, id, callback){
+    const update = 'update task set state = ? where id = ?';
+    connection.query(update, [state, id], (error, data) => {
+      if(error) callback(error, null);
+      callback(null, data);
+    })
+  }
+
+  deleteTask(id, callback){
+    const deleteT = `delete tasks where id = ${id}`;
+    connection.query(deleteT, (error, data) => {
+      if(error) callback(error, null);
+      callback(null, data);
+    })
+  }
 }
 
 module.exports = Task
